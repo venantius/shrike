7b29e813fdf167e9edab5ece56fe1886d3a7f009
David Jarvis <david.jarvis@airbnb.com>
2015-09-11 22:52:20 -0700

    Removing compiled JS from git

--- resources/public/js/out/figwheel/connect.js
+++
@@ -1,9 +0,0 @@
-// Compiled by ClojureScript 1.7.48 {}
-goog.provide('figwheel.connect');
-goog.require('cljs.core');
-goog.require('shrike.core');
-goog.require('figwheel.client');
-goog.require('figwheel.client.utils');
-figwheel.client.start.call(null,new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"build-id","build-id",1642831089),"app",new cljs.core.Keyword(null,"websocket-url","websocket-url",-490444938),"ws://localhost:3449/figwheel-ws"], null));
-
-//# sourceMappingURL=connect.js.map
