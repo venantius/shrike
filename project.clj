9f82e1e9734d02d33e9c0a5bb06af0fda6d7c303
David Jarvis <david.jarvis@airbnb.com>
2015-09-27 21:24:50 -0700

    Pointless commit

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject shrike "0.1.0-SNAPSHOT"
+(defproject shrike "0.1.0"
   :description "Hosted Code Quality Analysis for Clojure"
-  :url "http://example.com/FIXME"
+  :url "http://www.codeshrike.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
